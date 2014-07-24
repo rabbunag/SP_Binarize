f9ccdfa44c18ae02c4478f398f2b28be9cc2d039
rabbunag <rabbunag@gmail.com>
2014-07-24 20:59:33 +0800

    Rearranging the code and adding in line comments

--- SP_Binarize/SP_Binarize.cpp
+++ SP_Binarize/SP_Binarize.cpp
@@ -18,23 +18,32 @@
 using namespace cv;
 using namespace std;
 
+string windowName = "Manga";
+
 int main(int argc, char** argv)
 {
 	if (argv[1] == NULL){
 		printf("Include an image file");
 		return 0;
 	}
+
+	//image
 	Mat img = imread(argv[1], 1);
 	Mat img_gray;
 
+	//grayscale
 	cvtColor(img, img_gray, CV_BGR2GRAY);
 
+	//otsu's method
 	threshold(img_gray, img, 0, 255, CV_THRESH_OTSU);
-	imshow("img", img);
 
+	//window
+	namedWindow(windowName, WINDOW_AUTOSIZE);
+	imshow(windowName, img);
+
+	//naming and saving result
 	char * nameAndDestination = "";
 	char *next = NULL;
-
 	if (argv[2] == NULL) {
 		char * characterHandler;
 		characterHandler = strtok_s(argv[1], "\\", &next);
@@ -49,9 +58,9 @@ int main(int argc, char** argv)
 
 		imwrite(nameAndDestinationString, img);
 
-	} else {
+	}
+	else {
 		nameAndDestination = argv[2];
-
 		imwrite(nameAndDestination, img);
 	}
 	waitKey(0);
